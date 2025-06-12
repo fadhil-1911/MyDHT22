25d069392a3142eeec05792bb7950cd83a4c7376
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-06-12 22:47:30 +0800

    Update MyDHT22.h

--- src/MyDHT22.h
+++ src/MyDHT22.h
@@ -1,1 +1,18 @@
+#ifndef MYDHT22_H
+#define MYDHT22_H
 
+#include <Arduino.h>
+
+class MyDHT22 {
+  public:
+    MyDHT22(uint8_t pin);
+    bool readData();
+    float getTemperature();
+    float getHumidity();
+
+  private:
+    uint8_t _pin;
+    uint8_t data[5];
+};
+
+#endif
