22dbaf8ec51b1906e4c0b8d01f97ae626ca1b581
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-06-15 18:03:39 +0800

    Update BasicRead.ino

--- examples/BasicRead/BasicRead.ino
+++ examples/BasicRead/BasicRead.ino
@@ -1,17 +1,36 @@
-#include <MyDHT22.h>
+/*
+  BasicRead.ino - Basic example for reading temperature and humidity using DHT22
+  Library: MyDHT22 by Fadhil
+  Board: Arduino UNO / Nano
 
-MyDHT22 dht(2); // DHT22 on pin D2
+  Wiring:
+  - DHT22 DATA pin connected to digital pin 2
+  - Use a 10kΩ pull-up resistor between DATA and VCC
+*/
+
+#include <MyDHT22.h>  // Include the MyDHT22 library
+
+// Create a DHT22 sensor object on pin D2
+MyDHT22 dht(2);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  // Start the serial communication at 9600 baud
 }
 
 void loop() {
+  // Read the sensor data
   dht.read();
+
+  // Print temperature in Celsius
   Serial.print("Temperature: ");
   Serial.print(dht.getTemperature());
-  Serial.print(" °C, Humidity: ");
+  Serial.print(" °C, ");
+
+  // Print humidity in percentage
+  Serial.print("Humidity: ");
   Serial.print(dht.getHumidity());
   Serial.println(" %");
+
+  // Wait 2 seconds before the next reading
   delay(2000);
 }
