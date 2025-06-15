700d35aa89008250db3f31024a30aff1ed25a682
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-06-15 18:05:34 +0800

    Update Fahrenheit.ino

--- examples/Fahrenheit/Fahrenheit.ino
+++ examples/Fahrenheit/Fahrenheit.ino
@@ -1,20 +1,34 @@
-#include <MyDHT22.h>
+/*
+  FahrenheitRead.ino - Example for reading temperature in Fahrenheit from DHT22
+  Library: MyDHT22 by Fadhil
+  Board: Arduino UNO / Nano
 
+  Wiring:
+  - DHT22 DATA pin connected to digital pin 2
+  - Use a 10kΩ pull-up resistor between DATA and VCC
+*/
+
+#include <MyDHT22.h>  // Include the MyDHT22 library
+
+// Create a DHT22 sensor object on pin D2
 MyDHT22 dht(2);
 
+// Convert Celsius to Fahrenheit
 float toFahrenheit(float celsius) {
   return (celsius * 9.0 / 5.0) + 32;
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  // Start serial communication
 }
 
 void loop() {
+  // Read sensor and proceed only if successful
   if (dht.read()) {
     float tempC = dht.getTemperature();
     float tempF = toFahrenheit(tempC);
-    Serial.print("Temp: ");
+
+    Serial.print("Temperature: ");
     Serial.print(tempF);
     Serial.print(" °F, Humidity: ");
     Serial.print(dht.getHumidity());
@@ -22,5 +36,6 @@ void loop() {
   } else {
     Serial.println("Sensor error");
   }
-  delay(2000);
+
+  delay(2000); // Wait 2 seconds before next reading
 }
