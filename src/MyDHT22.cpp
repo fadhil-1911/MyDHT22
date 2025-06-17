5e52229c1c0c4e5dfd0f6d34e058074d7fbb50a1
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-06-17 10:15:48 +0800

    Update MyDHT22.cpp

--- src/MyDHT22.cpp
+++ src/MyDHT22.cpp
@@ -2,55 +2,62 @@
 
 MyDHT22::MyDHT22(uint8_t pin) {
   _pin = pin;
-  pinMode(_pin, INPUT);
+  pinMode(_pin, INPUT_PULLUP);
 }
 
 bool MyDHT22::readData() {
-  uint8_t lastState = HIGH;
-  uint8_t counter = 0;
-  uint8_t j = 0, i;
+  uint8_t byteIdx = 0, bitIdx = 7;
+  uint32_t t;
 
-  for (i = 0; i < 5; i++) data[i] = 0;
+  for (uint8_t i = 0; i < 5; i++) data[i] = 0;
 
+  // Minta data
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
-  delay(20);
+  delay(1); // minimum 1ms untuk DHT22
   digitalWrite(_pin, HIGH);
-  delayMicroseconds(40);
+  delayMicroseconds(30);
   pinMode(_pin, INPUT);
 
-  for (i = 0; i < 85; i++) {
-    counter = 0;
-    while (digitalRead(_pin) == lastState) {
-      counter++;
-      delayMicroseconds(1);
-      if (counter == 255) break;
-    }
+  // Tunggu respon DHT22: LOW 80us + HIGH 80us
+  t = micros();
+  while (digitalRead(_pin) == HIGH)
+    if ((micros() - t) > 90) return false;
 
-    lastState = digitalRead(_pin);
-    if (counter == 255) break;
+  t = micros();
+  while (digitalRead(_pin) == LOW)
+    if ((micros() - t) > 90) return false;
 
-    if ((i >= 4) && (i % 2 == 0)) {
-      data[j / 8] <<= 1;
-      if (counter > 40) data[j / 8] |= 1;
-      j++;
+  t = micros();
+  while (digitalRead(_pin) == HIGH)
+    if ((micros() - t) > 90) return false;
+
+  // Terima 40 bit (5 byte)
+  for (uint8_t i = 0; i < 40; i++) {
+    while (digitalRead(_pin) == LOW); // tunggu HIGH
+    t = micros();
+    while (digitalRead(_pin) == HIGH); // tunggu habis HIGH
+    if ((micros() - t) > 40) data[byteIdx] |= (1 << bitIdx);
+    if (bitIdx == 0) {
+      bitIdx = 7;
+      byteIdx++;
+    } else {
+      bitIdx--;
     }
   }
 
-  if (j >= 40) {
-    uint8_t checksum = data[0] + data[1] + data[2] + data[3];
-    return ((checksum & 0xFF) == data[4]);
-  }
-
-  return false;
+  // Semak checksum
+  uint8_t chk = data[0] + data[1] + data[2] + data[3];
+  return (chk == data[4]);
 }
 
 float MyDHT22::getHumidity() {
-  return ((data[0] << 8) + data[1]) * 0.1;
+  return ((data[0] << 8) | data[1]) * 0.1;
 }
 
 float MyDHT22::getTemperature() {
-  float t = (((data[2] & 0x7F) << 8) + data[3]) * 0.1;
+  int16_t raw = ((data[2] & 0x7F) << 8) | data[3];
+  float t = raw * 0.1;
   if (data[2] & 0x80) t = -t;
   return t;
 }
